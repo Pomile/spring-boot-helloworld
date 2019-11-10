e6c348775e61c9de2cb325017d95078bc592370b
Babatunde <soft-sky@live.co.uk>
2019-11-10 15:49:58 +0100

    Create a filter

--- src/main/java/com/spring01/springboothelloworld/HelloServlet.java
+++ src/main/java/com/spring01/springboothelloworld/HelloServlet.java
@@ -5,10 +5,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
 @WebServlet(name="helloServlet", urlPatterns ="/helloServlet")
+
 public class HelloServlet extends HttpServlet{
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) {
         System.out.println("Running Hello Servlet doGet method");
     }
 }
+
